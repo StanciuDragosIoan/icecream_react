e982f5a12a468d3328d296168cae8b3549192f43
Dragos <Dragos.Dragos@honeywell.com>
2019-06-29 01:24:40 +0300

    test

--- client/src/components/pages/Addicecream.js
+++ client/src/components/pages/Addicecream.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+// import axios from "axios";
 
 const Addicecream = () => {
   //use state hook
@@ -11,18 +12,51 @@ const Addicecream = () => {
 
   //destructure state
   const { flavour, price, description, quantity } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    if (!flavour) {
+      console.log("There is an issue with the data...");
+    } else {
+      // // ###request without redux
+      // const newIcecream = formData;
+
+      // try {
+      //   //config object holds the http headers
+      //   const config = {
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     }
+      //   };
+
+      //   //data body
+      //   const body = JSON.stringify(newIcecream);
+
+      //   //response
+      //   const res = await axios.post("/api/icecream", body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.log(err.response.data);
+      // }
+      console.log("Success");
+    }
+  };
   return (
     <Fragment>
       <div className="container">
         <h3 className="mt-5">Add new icecream flavours to our shop!:D</h3>
-        <form>
+        <form onSubmit={e => onSubmit(e)}>
           <div className="form-group">
             <input
               type="text"
               className="form-control"
               placeholder="What's your flavour?"
               name="flavour"
+              value={flavour}
+              onChange={e => onChange(e)}
               required
             />
           </div>
@@ -32,6 +66,8 @@ const Addicecream = () => {
               className="form-control"
               placeholder="Price per piece?"
               name="price"
+              value={price}
+              onChange={e => onChange(e)}
             />
           </div>
           <div className="form-group">
@@ -40,6 +76,8 @@ const Addicecream = () => {
               className="form-control"
               placeholder="What does it taste like?"
               name="description"
+              value={description}
+              onChange={e => onChange(e)}
             />
           </div>
           <div className="form-group">
@@ -48,6 +86,8 @@ const Addicecream = () => {
               className="form-control"
               placeholder="How many pieces do you have in stock?"
               name="quantity"
+              value={quantity}
+              onChange={e => onChange(e)}
             />
           </div>
           <input
--- client/src/components/pages/Buyicecream.js
+++ client/src/components/pages/Buyicecream.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Buyicecream = () => {
-  return <div>Buy your favourite icecream</div>;
+  return (
+    <div className="card p-5 col-sm-8 mx-auto mt-5">
+      <h1 className="mt-5 text-center  mb-5">
+        Buy your favourite icecream from below!
+      </h1>
+      <div className="col-sm-8 mx-auto bg-danger">Vending Machine is here</div>
+    </div>
+  );
 };
 
 export default Buyicecream;
